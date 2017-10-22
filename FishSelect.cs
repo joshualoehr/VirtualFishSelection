fa9fc41f8e1c0084dfb4b6cf23a2f23c48a1d4d8
Joshua Loehr <loehrj@students.wwu.edu>
2017-10-21 17:18:45 -0700

    Added Bottom3Select and placeholders for all other select strategies

--- FishSelect.cs
+++ FishSelect.cs
@@ -22,6 +22,24 @@ namespace FishEvolutionGenetic
                 case TOP_3_SELECT:
                     SelectStrategy = new Top3Select();
                     break;
+                case BOTTOM_3_SELECT:
+                    SelectStrategy = new Bottom3Select();
+                    break;
+                case FITNESS_PROP_SELECT:
+                    SelectStrategy = new FitnessPropSelect();
+                    break;
+                case ROULETTE_SELECT:
+                    SelectStrategy = new RouletteSelect();
+                    break;
+                case TOURNAMENT_SELECT:
+                    SelectStrategy = new TournamentSelect();
+                    break;
+                case STEADY_STATE_SELECT:
+                    SelectStrategy = new SteadyStateSelect();
+                    break;
+                case ELITISM_SELECT:
+                    SelectStrategy = new ElitismSelect();
+                    break;
                 default:
                     SelectStrategy = new Top3Select();
                     break;
@@ -55,6 +73,93 @@ namespace FishEvolutionGenetic
                 else
                     fish[i] = new Fish(FishChromozomes.Mate(selectedFish.ElementAt(1).Chromozomes, selectedFish.ElementAt(2).Chromozomes));
             }
+        }
+    }
+
+    class Bottom3Select : ISelectStrategy
+    {
+        public List<Fish> SelectFish(List<Fish> fish)
+        {
+            List<Fish> topFish = fish.OrderBy(r => r.NumFoodEaten).Take(3).ToList();
+            return topFish;
+        }
+
+        public void BreedFish(List<Fish> fish, List<Fish> selectedFish)
+        {
+            for (int i = 0; i < fish.Count; i++)
+            {
+                if (i < fish.Count / 3)
+                    fish[i] = new Fish(FishChromozomes.Mate(selectedFish.ElementAt(0).Chromozomes, selectedFish.ElementAt(1).Chromozomes));
+                else if (i < fish.Count * 2 / 3)
+                    fish[i] = new Fish(FishChromozomes.Mate(selectedFish.ElementAt(0).Chromozomes, selectedFish.ElementAt(2).Chromozomes));
+                else
+                    fish[i] = new Fish(FishChromozomes.Mate(selectedFish.ElementAt(1).Chromozomes, selectedFish.ElementAt(2).Chromozomes));
+            }
+        }
+    }
+
+    class FitnessPropSelect : ISelectStrategy
+    {
+        public List<Fish> SelectFish(List<Fish> fish)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void BreedFish(List<Fish> fish, List<Fish> selectedFish)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    class RouletteSelect : ISelectStrategy
+    {
+        public List<Fish> SelectFish(List<Fish> fish)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void BreedFish(List<Fish> fish, List<Fish> selectedFish)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    class TournamentSelect : ISelectStrategy
+    {
+        public List<Fish> SelectFish(List<Fish> fish)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void BreedFish(List<Fish> fish, List<Fish> selectedFish)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    class SteadyStateSelect : ISelectStrategy
+    {
+        public List<Fish> SelectFish(List<Fish> fish)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void BreedFish(List<Fish> fish, List<Fish> selectedFish)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    class ElitismSelect : ISelectStrategy
+    {
+        public List<Fish> SelectFish(List<Fish> fish)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void BreedFish(List<Fish> fish, List<Fish> selectedFish)
+        {
+            throw new System.NotImplementedException();
         }
     }
 
